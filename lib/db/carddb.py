3c0967f1bf91bb646b611c6478e6e45ec5fb0b09
Reid McKenzie <rmckenzie92@gmail.com>
2012-02-06 13:56:45 -0600

    Added a simple abstraction for the database file

---
+++ lib/db/carddb.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from ..cards.card import Card
+
+class CardDB():
+    __cards__           = []    # list of Card objects
+    __names_to_ind__    = {}    # map of '':Card
+    
+    def __init__(self, file='./cards.dat'):
+        f = open(file,'r')
+        for line in f:
+            try:
+                eval("d = "+line)
+                d = Card(None, d)
+                self.__cards__.append(d)
+                self[d.__dict__[u'Card Name:']] =  len(self.__cards__
+
+            except Exception:
+                continue
+
+    def __contains__(self, element):
+        """element is assumed to be the name of a card, or a card instance"""
+        return ((element in self.__names_to_int__) 
+                or (element in self.__cards__))
+
+    def __getitem__(self, addr):
+        """addr is assumed to be a valid card name"""
+        if addr in self.__names_to_ind__:
+            return self.__cards__[self.__names_to_ind__[addr]]
+        else:
+            return None
+
+    def __setitem__(self, addr, value):
+        """addr is assumed to be a valid card name, value a card instance"""
+        self.__cards__.append(value)
+        self.__names_to_ind__[u'Card Name:'] = len(self.__cards__)
+
+    def search(self, prop, value):
+        return [c for c in self.__cards__ 
+                    if (prop in c.__dict__ and c.__dict__[prop] == value)]
+
+
