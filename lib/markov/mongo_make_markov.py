1db8f7c6cc990cedffa3fa4317bdff0c7791a611
Reid McKenzie <rmckenzie92@gmail.com>
2012-03-21 16:11:02 -0500

    Moved a setup tool out of the runables directory

---
+++ lib/markov/mongo_make_markov.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+from pymongo.connection import Connection
+connection = Connection("146.6.213.39")
+db = connection.magic
+
+start = int(db.decks.find().count() * 0.416653800017)
+cursor = db.decks.find().skip(start)
+c = start
+while 1:
+    try:
+        for deck in cursor:
+            print deck['_id'], float(c)/float(cursor.count())
+            c += 1
+            duck = {k:deck[k] for k in deck if k not in ['name', '_id']}
+            db.markov.update({'name': {"$in" : [k for k in deck
+                                                if k not in ['_id', 'name']]}},
+                             {'$inc': duck}, True)
+        exit(0)
+    except:
+        cursor = db.decks.find().skip(int(c)) # continue from where the run failed
+
+
