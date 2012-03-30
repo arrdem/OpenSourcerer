4c7bbbf40d0b2d6ed206f79250852c25f1a346ad
Reid McKenzie <rmckenzie92@gmail.com>
2012-03-30 01:19:28 -0500

    Added a config file to prevent futher leakage pf information

--- essentialmagic_server-2.py
+++ essentialmagic_server-2.py
@@ -1,147 +1,238 @@
 #!/usr/bin/env python2
+# This file defines the server side of a distributed gatherer-scraping system
 
+from lib.network.distributed_server import Server
+from lib.network.distributed_server import connectionThread
 from pymongo.connection import Connection
-import socket
-import threading
+from ConfigParser import SafeConfigParser
+from itertools import chain
+import curses
+import urllib
 import pickle
-import string
 import time
 import re
 
-global db
-connection = Connection("146.6.213.39")
-db = connection.magic
+global parser
+parser = SafeConfigParser()
+parser.read('settings.ini')
 
-
-class connectionThread(threading.Thread):
+class GathererThread(connectionThread):
 
     def __init__(self, conn):
-        threading.Thread.__init__(self)
-        self.__conn__   = conn
-        self.__running__= True
-        self.__last__   = 0
-        self.__client__ = socket.gethostbyaddr(self.__conn__.getpeername()[0])[0]
+        connectionThread.__init__(self, conn)
+        self.__verbose__ = False
+        self.__count__ = 0
+        self.__failures__ = 0
+        self.__fail_count__ = 0
+        self.__fail_limit__ = 10
+        self.__s_time__ = time.time()
 
-    def join(self):
-        self.__running__ = False
-        self.__conn__.send("DONE".encode())
-        print("[ %30s ] Thread killed..." % (self.__client__))
-        try:
-            threading.Thread.join(self)
-        except:
-            return
+        global x, y
+        self.__cursor_x__ = x
+        self.__cursor_y__ = y
+        y += 1
+
+    def _print(self, *args):
+        global outpad
+        for a in args:
+            outpad.addstr(self.__cursor_y__,
+                          self.__cursor_x__,
+                          " %s" % (str(a),))
+        #outpad.border('|', '|')
+        outpad.refresh()
+
+    def send(self, string):
+        self.__conn__.send(string.decode())
+
+    def status(self):
+        self._print(" %30s Errors: %i    Successes: %i    Pageid: %i    Status: %s" %
+                    (self.__client__, self.__failures__, self.__count__,
+                     self.__last__, str(self.__running__)))
 
     def run(self):
-        global ticker
+        global generator
         data = " "
+        connection = Connection(parser.get('mongodb', 'server'))
+        db = None
+        exec('db = connection.' + parser.get('mongodb', 'db'))
+
         while self.__running__ and data:
-            data = str(self.__conn__.recv(4096)).decode("utf-8", errors='ignore')
-            #print("[", self.ident, "] UPKEEP SIGNAL FROM", self.__conn__.getpeername())
+            data = str(self.__conn__.recv(1024*32)).decode("utf-8",
+                                                            errors='ignore')
+            self.status()
 
-            if(ticker >= limit):
-                self.__conn__.send(str("DONE").encode())
-                self.join()
-
-            elif data == "NEXT":
-                ticker += 1
+            if data == "NEXT":
+                ticker = generator.next()
                 self.__last__ = ticker
-                self.__conn__.send(str(ticker).encode())
+                self.__conn__.send(str("PAGEID %i" % (ticker)).encode())
 
             elif "FAIL" in data:
                 i = int(re.sub("FAIL ", '', data))
-                #print("[ %30s ] FAILED TO DOWNLOAD DECK ID %i" % (self.__client__,  i))
+                generator.logfail(self.__last__)
+
+            elif "ERROR" in data:
+                self._print("%-100s" % (" %30s %s" % (self.__client__, data)))
+
+            elif "FATAL" in data:
+                self._print("%-100s" % (" %30s NODE DOWN '%s'" % (self.__client__, data)))
+                self.join()
 
             elif "OKAY" in data:
-                # this is the signal from the client that a new deck has
-                # been found. Format is as follows:
-                # DECK (bytes)
-                # followed by the pickled dict
-                try:
-                    i = int(re.sub("OKAY ", '', data))
-                    self.__conn__.send('OK'.encode())
-                except:
-                    continue
+                if(self.__fail_count__ < self.__fail_limit__):
+                    try:
+                        i = int(data.split(' ',1)[1])
+                        self.__conn__.send('GOAHEAD'.encode())
+                    except Exception as e:
+                        exit(1)
 
-                deck = None
-                try:
-                    deck = pickle.loads(self.__conn__.recv(i))
+                    card = None
+                    try:
+                        card = pickle.loads(self.__conn__.recv(i))
+                        self.__fail_count__ = 0
+                        self.__conn__.send('1'.encode())
+                        self.__count__ += 1
+                    except Exception as e:
+                        self.__fail_count__ += 1
+                        self.__conn__.send('0'.encode())
+
+                    if card is not None:
+                        card['_id'] = str(self.__last__)
+                        db.cards.insert(card)
+
+                        self._print("%-100s" % (" %30s DOWNLOADED CARD %i : %s" % \
+                                (self.__client__, self.__last__, card['name'])))
+                else:
                     self.__conn__.send('1'.encode())
-                except:
-                    self.__conn__.send('0'.encode())
-
-                if deck is not None:
-                    deck['sum'] = sum([deck[i] for i in deck if isinstance(deck[i], int)])
-                    deck['_id'] = str(self.__last__)
-
-                    db.decks.insert(deck)
-
-                    print("[ %30s ] DOWNLOADED DECK %i" % \
-                             (self.__client__, self.__last__))
+                    self.__fail_count__ = 0
+                    self.__failures__ += 1
+                    self._print("%-100s" % (" %30s FAILES TO DOWNLOADED CARD %i" % \
+                                 (self.__client__, self.__last__)))
 
         if(not data):
-            print("[ %30s ] NO TRAFFIC, EXITING" % (self.__client__))
+            self._print("%-100s" % (" %30s NO TRAFFIC, EXITING" % (self.__client__)))
+            self.join()
 
 
-class Server(object):
-    __thread__  = None
-    __clients__ = []
-    __host__    = ''
-    __port__    = 0
-    __buff__    = 0
-    __addr__    = ''
-    __up__      = False
-    __sock__    = None
-    __count__   = 0
+class GathererServer(Server):
 
-    def __init__(self, host='', port=9001, buff=1024):
-        self.__host__   = host
-        self.__port__   = port
-        self.__buff__   = buff
-        self.__addr__   = (self.__host__, self.__port__)
-        self.__up__     = True
+    def __init__(self, *args, **kwargs):
+        Server.__init__(self, *args, **kwargs)
 
-        self.__sock__   = socket.socket()
-        self.__sock__.bind(self.__addr__)
-        self.__sock__.settimeout(1.0)
-        self.__sock__.listen(2)
+    def handleNewClient(self, client):
+        global clients
+        #print "Client connected from {}.".format(client[1])
+        handler = GathererThread(client[0])
+        clients[handler.__client__] = handler
+        self.addHandler(handler)
+        handler.start()
 
-    def run(self):
-        if self.__thread__:
-            return
+class MUIDItterator():
+    __seed_MIDs__= list(map(int, parser.get('scrape', 'seeds').split(',')))
+
+    def __init__(self):
+        self.__used_muids__ = set()
+        self.__last_muid__ = None
+        self.__last_random_muid__ = None
+        self.__delta__ = -1
+        self.__fail_count__ = 0
+        self.__fail_limit__ = 25
+        self.__last__ = 0
+        self.__gen__ = chain(self.__seed_MIDs__, self.getRandomMUID())
+
+    def __iter__(self):
+        return self.__last__
+
+    def logfail(self, i):
+        self.__fail_count__ += 1
+
+    def next(self):
+        if(self.__last__ == 0):
+            self.__last__ = next(self.__gen__)
+
+        elif(self.__fail_count__ < self.__fail_limit__):
+            n = self.__last__ + self.__delta__
+            if n in self.__used_muids__:
+                self.__last__ = next(self.__gen__)
+            else:
+                self.__last__ = n
+
+        elif(self.__fail_count__ >= self.__fail_limit__) and (not self.__delta__):
+            self.__fail_count__ = 0
+            self.__last__ = self.__last_muid__ + 1
+            self.__delta__ = 1
+
         else:
-            self.__thread__ = threading.Thread(target=self.__run__)
-            self.__thread__.start()
+            self.__fail_count__ = 0
+            self.__last__ = next(self.__gen__)
 
-    def stop(self):
-        if self.__thread__:
-            for t in self.__clients__:
-                t.join()
-                print("Killed thread", t)
-            self.__up__ = False
-            self.__thread__.join()
-            print("Thread killed...")
+        self.__used_muids__.add(self.__last__)
+        return self.__last__
 
-        else:
-            print("Server was not running..")
 
-    def __run__(self):
-        print("Awaiting connections. . .")
-        while self.__up__:
+    def getRandomMUID(self):
+        opener = urllib.build_opener(urllib.HTTPErrorProcessor(),
+                                     urllib.HTTPRedirectHandler(),
+                                     urllib.HTTPHandler())
+        request = urllib.Request("http://gatherer.wizards.com/Pages/Card/Details.aspx?action=random")
+        c = 0
+        while 1:
             try:
-                client      = self.__sock__.accept()
-                print("Client connected from {}.".format(client[1]))
+                response = opener.open(request)
+            except Exception as e:
+                i = int(e.__dict__['url'].split('=')[1])
+                if i not in self.__used_muids__:
+                    yield i
+                elif(c > 100):  # this means at least 99.9% completion.
+                    raise StopIteration()
+                else:
+                    c += 1
 
-                self.__clients__.append(connectionThread(client[0]))
-                self.__clients__[-1].start()
 
-            except socket.timeout:
-                continue
+if __name__ == "__main__":
+    global generator
+    generator = MUIDItterator()
 
-if __name__ == "__main__" or 1:
-    global ticker, limit
-    ticker = 1
-    limit = 920000
-    serv = Server()
+    stdscr = curses.initscr()
+    curses.echo()
+    curses.cbreak()
+    curses.setsyx(150, 1)
+
+    global outpad
+    outpad = curses.newwin(115, 100, 0, 0)
+    inpad = curses.newwin(40, 50, 0, 101)
+
+    inpad.scrollok(True)
+    inpad.idlok(True)
+
+    outpad.scrollok(True)
+    outpad.idlok(True)
+
+    global x, y
+    x, y = 1, 1
+
+    serv = GathererServer()
     serv.run()
-    raw_input()
-    serv.stop()
+
+    global clients
+    clients = {}
+
+    inpad.addstr('\n')
+    while 1:
+        inpad.addstr(' $ ')
+        inpad.border('|', '|')
+        inpad.refresh()
+        cmd = str(inpad.getstr()).decode('utf-8', errors='ignore').split(' ', 1)
+
+        if cmd[0] == 'send':
+            conn.send(cmd[1].encode())
+
+        elif cmd[0] == 'exit':
+            serv.stop()
+            curses.endwin()
+            break
+
+        else:
+            try:
+                exec(' '.join(cmd))
+            except Exception as e:
+                print(e)
